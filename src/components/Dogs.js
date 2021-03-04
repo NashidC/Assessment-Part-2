46bfda2b5e74a174ac8bd997945ef12d3e07af01
NashidC <nash.chowdhury@gmail.com>
2021-03-03 20:13:18 -0500

    init, images load

---
+++ src/components/Dogs.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+
+function Dogs() {
+
+
+
+  const [dog, setDog] = useState({});
+  const url = "https://dog.ceo/api/breeds/image/random"
+
+  const generateImage = async () => {
+    const response = await axios.get(url);
+    console.log(response)
+    setDog(response.data.message)
+  }
+  useEffect(() => {
+    generateImage()
+  }, [])
+
+
+  return (
+    <div>
+
+      <h3>Generate a random dog</h3>
+      <button onClick={() => generateImage()}>Dog Me!</button>
+      <br />
+      <img src={dog} />
+    </div>
+  );
+}
+
+export default Dogs;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => (
+  <div className="footer">
+    Created by <a href="https://github.com/nashidc">@nashidc</a>
+  </div>
+);
